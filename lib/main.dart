70ec9ad4659b095337908f87090d945b7522404a
nikunj <nik303unj@gmail.com>
2024-01-04 11:04:22 +0530

    ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -54,61 +54,106 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              "Sqlite",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
                   controller: id,
                   decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
                     labelText: 'Id',
+                    hintText: '123',
                   ),
-                  keyboardType: TextInputType.number),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextField(
-                controller: desc,
-                decoration: const InputDecoration(
-                  labelText: 'Description',
+                  keyboardType: TextInputType.number,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (id.text.isNotEmpty && desc.text.isNotEmpty) {
-                    _sqliteService.createItem(
-                        Note(id: int.parse(id.text), description: desc.text));
-                  }
-                  _refreshNotes();
-                  id.clear();
-                  desc.clear();
-                },
-                child: const Text("Add"),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: _notes.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () async {
-                          await _sqliteService
-                              .deleteItem(_notes[index].id.toString());
-                          _refreshNotes();
-                        },
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  controller: desc,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
                       ),
-                      title: Text(_notes[index].id.toString()),
-                      subtitle: Text(_notes[index].description),
-                    );
-                  }),
-            )
-          ],
+                    ),
+                    labelText: 'Description',
+                    hintText: 'Write message',
+                  ),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (id.text.isNotEmpty && desc.text.isNotEmpty) {
+                      _sqliteService.createItem(
+                          Note(id: int.parse(id.text), description: desc.text));
+                    }
+                    _refreshNotes();
+                    id.clear();
+                    desc.clear();
+                  },
+                  child: const Text("Add"),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "List of notes",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              _notes.isEmpty
+                  ? const Expanded(
+                      child: Center(
+                        child: Text(
+                          "No records found",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Expanded(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: _notes.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              trailing: IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () async {
+                                  await _sqliteService
+                                      .deleteItem(_notes[index].id.toString());
+                                  _refreshNotes();
+                                },
+                              ),
+                              title: Text(_notes[index].id.toString()),
+                              subtitle: Text(_notes[index].description),
+                            );
+                          }),
+                    ),
+            ],
+          ),
         ),
       ),
     );
